6a6d26adb8c5a274320d70af0b1a0f395a16a963
ievsepl <107007762+ievsepl@users.noreply.github.com>
2023-03-17 17:03:46 +0200

    remove comments

--- server.js
+++ server.js
@@ -1,9 +1,5 @@
 const app = require("./app");
-// require("dotenv").config();
 const mongoose = require("mongoose");
-
-// const DB_HOST =
-// "mongodb+srv://ievsepl:50513970@cluster0.clrteox.mongodb.net/db-contacts?retryWrites=true&w=majority";
 
 const { PORT = 3000, DB_HOST } = process.env;
 mongoose
